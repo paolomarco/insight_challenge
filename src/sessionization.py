08e0967a0c14b14e9d6abeb31bc49d67ae78b1ec
Tobia Martens <tobiamartens2016@u.northwestern.edu>
2018-06-14 11:12:01 -0500

    Update sessionization.py
    Added time change check. Added functions.

--- src/sessionization.py
+++ src/sessionization.py
@@ -1,4 +1,5 @@
 
+
 # coding: utf-8
 
 # import libraries
@@ -6,14 +7,15 @@ import pandas
 from datetime import datetime
 import sys
 import os
+import csv
 
 # import arguments from the command line
-log_file = sys.argv[1]
+input_file = sys.argv[1]
 inactivity_file = sys.argv[2]
 output_file = sys.argv[3]
 
-# import data
-log_file = pandas.read_csv(log_file, 
+def import_log():
+    log_file = pandas.read_csv(input_file, 
                            header=0,
                            dtype = {'cik': object},
                            parse_dates = {'datetime':['date','time']},
@@ -23,40 +25,68 @@ log_file = pandas.read_csv(log_file,
                                     "cik",
                                     "accession",
                                     "extention"])
+    return log_file
 
-with open(inactivity_file) as file:
-    inactivity_period = int(file.read())
-    file.close()
+def import_inactivity():
+    with open(inactivity_file) as file:
+        inactivity_period = int(file.read())
+        file.close()
+    return inactivity_period    
+    
+def data_processing(df):
+    df['doc_name'] = df.cik + df.accession + df.extention
+    df = df.drop(['cik', 'accession','extention'], axis=1)
+    return df
 
-# data formatting
-log_file['doc_name'] = log_file.cik + log_file.accession + log_file.extention
+def write_output(output_list):
 
-log_file = log_file.drop(['cik', 'accession','extention'], axis=1)
+    fileout = output_file
 
-# create empty dictionary to store ip session data
+    if os.path.exists(fileout):
+        append_write = 'a' # append if already exists
+    else:
+        append_write = 'w' # make a new file if not
+
+    with open(fileout, append_write) as f:
+        writer = csv.writer(f)
+        writer.writerows(output_list)
+
+    f.close()
+
+# import data
+log_file = import_log()
+inactivity_period = import_inactivity()
+
+# process data
+log_file = data_processing(log_file)
+
+start_time = datetime.now()
+output_list = []
 ip_dict = {}
-
-# file to write session information to
-fileout = output_file
-
-if os.path.exists(fileout):
-    append_write = 'a' # append if already exists
-else:
-    append_write = 'w' # make a new file if not
-
-output_file = open(fileout,
-                   append_write)
-
 counter = 0
+prev_timestamp = None
 
 # iterate over log_file
-for row in log_file.itertuples():
+for row in log_file.itertuples(): # this outputs a tuple 
+    
+    counter += 1
     
     # retrieve ip address & time stamp of request
-    counter = counter + 1
     ip_address = row[2]
     current_timestamp = row[1]
-            
+    
+    # detect time change
+    if prev_timestamp is not None: 
+        sec_diff = prev_timestamp - current_timestamp
+        if sec_diff.seconds > 0:
+            check_dict = True
+            prev_timestamp = current_timestamp # assign new prev_timestmap
+        else:
+            check_dict = False
+    else:
+        prev_timestamp = current_timestamp
+        check_dict = False
+
     # check if ip_address has made document request & if time elapsed since
     # exceeds session threshold else increment documents requested by 1
     if ip_address in ip_dict:
@@ -71,7 +101,7 @@ for row in log_file.itertuples():
                      str(current_timestamp),
                      duration,
                      ip_dict[ip_address]['n_doc']]
-            output_file.write("%s\n" % ','.join(str(i) for i in output))
+            output_list.append(output)
             del ip_dict[ip_address]
         else:
             ip_dict[ip_address]['last_request'] = current_timestamp
@@ -83,11 +113,10 @@ for row in log_file.itertuples():
         ip_dict[ip_address]['first_request'] = current_timestamp
         ip_dict[ip_address]['last_request'] = current_timestamp
         ip_dict[ip_address]['n_doc'] = 1
-        ip_dict[ip_address]['index'] = counter
+        ip_dict[ip_address]['index'] = row[0]
     
     # close out all sessions if last row of log_file
     if len(log_file.index) == counter:
-        
         ip_address_list = []
         for _ip_adress in list(ip_dict):
             ip_address_list.append(_ip_adress)
@@ -102,25 +131,34 @@ for row in log_file.itertuples():
                      str(ip_dict[_ip_adress]['last_request']),
                      duration,
                      ip_dict[_ip_adress]['n_doc']]
-            output_file.write("%s\n" % ','.join(str(i) for i in _output))
+            output_list.append(_output)
             del ip_dict[_ip_adress]
     
     # check if any recorded sessions in dictionary exceed session time treshold
-    for _ip_adress in list(ip_dict):
-        last_request = ip_dict[_ip_adress]['last_request']
-        time_elapsed = current_timestamp - last_request
-        if time_elapsed.seconds > inactivity_period:
-            duration = (last_request - ip_dict[_ip_adress]['first_request'])
-            duration = duration.seconds + 1
-            _output = [_ip_adress,
-                     str(ip_dict[_ip_adress]['first_request']),
-                     str(last_request),
-                     duration,
-                     ip_dict[_ip_adress]['n_doc']]
-            output_file.write("%s\n" % ','.join(str(i) for i in _output))
-            del ip_dict[_ip_adress]
+    if check_dict == True:
+        for _ip_adress in list(ip_dict):
+            last_request = ip_dict[_ip_adress]['last_request']
+            time_elapsed = current_timestamp - last_request
+            if time_elapsed.seconds > inactivity_period:
+                duration = (last_request - ip_dict[_ip_adress]['first_request'])
+                duration = duration.seconds + 1
+                _output = [_ip_adress,
+                         str(ip_dict[_ip_adress]['first_request']),
+                         str(last_request),
+                         duration,
+                         ip_dict[_ip_adress]['n_doc']]
+                output_list.append(_output)
+                del ip_dict[_ip_adress]
+            
+# write results to output file
+write_output(output_list)
+            
+end_time = datetime.now()
 
-output_file.close()
+sessionization_time = end_time - start_time
+print("Sessionization took ", sessionization_time," seconds")
+
+
 
 
 
